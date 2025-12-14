c4f6b6bfe87198a983bd6919073f71fbd65121b3
Tu Nombre <bismartlimamanseda@gmail.com>
2025-12-14 15:00:27 -0400

    Configuracion github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://reinicio333.github.io',
-  base: 'procerbolsai_astro',
+  base: '/procerbolsai_astro',
   vite: {
     plugins: [tailwindcss()]
   }
